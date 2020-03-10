3f755c14c09e12422438d0fc7818380001931e7c
beatriz-perez <beatriz.pfp@gmail.com>
2020-03-10 17:47:11 +0100

    add js functions to make the game work

--- js/main.js
+++ js/main.js
@@ -1,2 +1,48 @@
 'use strict';
 
+// Generamos el número elegido al cargar la página
+
+function getRandomNumber(max) {
+    return Math.ceil(Math.random() * max);
+}
+
+const winnerNumber = getRandomNumber(100);
+console.log(`estoy pensando en el número ${winnerNumber}, intenta adivinarlo!`);
+
+// Establecemos las reglas del juego con la función checkNumber y registramos cada intento con countRounds
+
+const numberInput = document.querySelector('#number');
+const tryButton = document.querySelector('#button');
+const hintBox = document.querySelector('.hint');
+const countBox = document.querySelector('.count');
+let round = 0;
+countBox.innerHTML = round;
+
+
+function checkNumber () {
+    const myNumber = parseInt(numberInput.value);
+    if (myNumber === winnerNumber) {
+        hintBox.innerHTML = 'Has ganado campeona!!!';
+        tryButton.classList.add('hidden') // Al ganar el botón desaparece para detener el juego
+    } else if (myNumber < winnerNumber && myNumber > 0) {
+        hintBox.innerHTML = 'Demasiado bajo';
+    } else if (myNumber > winnerNumber && myNumber < 101) {
+        hintBox.innerHTML = 'Demasiado alto';
+    } else {
+        hintBox.innerHTML = 'El número debe estar entre 1 y 100';
+    }
+}
+function countRounds () {
+    round ++;
+    countBox.innerHTML = round;
+}
+
+
+function tryButtonHandler (event) {
+    event.preventDefault();
+    checkNumber();
+    countRounds();
+}
+
+
+tryButton.addEventListener('click', tryButtonHandler)
